86611bff586a26052a787ce191aaf5c27e0a2209
alfikiyarhaidar <alfikiyarhaidar@gmail.com>
2022-05-06 20:00:10 +0700

    Penggabungan Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fashionizt/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:fashionizt/Pages/home_pages.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePages(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:fashionizt/pages/home_pages.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+
+  void initState(){
+   super.initState();
+   Timer(Duration(seconds: 3),() {
+     Navigator.pushReplacement(
+         context, MaterialPageRoute(builder: (context) => HomePages()));
+   });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xFFFAF3E0),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/Assets/images/fashionizt.jpeg',
+              width: 450,
+              height: 600,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
