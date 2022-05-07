1e096c57c16e45b482b233ea85f9c3113cd5880f
TeamScrumbB1 <teamscrumb1@gmail.com>
2022-05-07 20:53:08 +0700

    Cart

---
+++ lib/pages/cart_screen.dart
@@ -0,0 +1,53 @@
+import 'package:fashionizt/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:fashionizt/Models/Cart.dart';
+
+import 'components/body.dart';
+import 'components/check_out_card.dart';
+import '../../../size_config.dart';
+
+class CartScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    {
+      return Scaffold(
+        appBar: buildAppBar(context),
+        body: Body(),
+        bottomNavigationBar: CheckoutCard(),
+      );
+    }
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      centerTitle: false,
+      titleSpacing: 0,
+        leading: GestureDetector(
+          onTap: () {},
+          child: IconTheme(
+            data: new IconThemeData(color: Colors.black),
+            child: new Icon(Icons.arrow_back_ios_rounded),
+          ),
+        ),
+      backgroundColor: darkYellowColor,
+      title: Column(
+        children:[
+          Text.rich(
+            TextSpan(
+              text: "Your Cart\n",
+              style: TextStyle(fontFamily: 'Poppins', fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(
+                  text: "${demoCarts.length} items",
+                  style: Theme.of(context).textTheme.caption,
+                ),
+              ],
+            ),
+          ),
+        ]
+      ),
+      );
+  }
+}
---
+++ lib/pages/components/check_out_card.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:fashionizt/Widget/default_button.dart';
+
+
+import '../../../size_config.dart';
+
+class CheckoutCard extends StatelessWidget {
+  const CheckoutCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    {
+      return Container(
+        padding: EdgeInsets.symmetric(
+          vertical: getProportionateScreenWidth(15),
+          horizontal: getProportionateScreenWidth(30),
+        ),
+        // height: 174,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, -15),
+              blurRadius: 20,
+              color: Color(0xFFDADADA).withOpacity(0.15),
+            )
+          ],
+        ),
+
+        child: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: getProportionateScreenHeight(10)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      text: "Total:\n",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500),
+                      children: [
+                        TextSpan(
+                          text: "\Rp. 639.000",
+                          style: TextStyle(fontFamily: 'Poppins', fontSize: 16, color: Colors.black, fontWeight: FontWeight.w800),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: getProportionateScreenWidth(190),
+                    child: DefaultButton(
+                      text: "Check Out",
+                      press: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+}
