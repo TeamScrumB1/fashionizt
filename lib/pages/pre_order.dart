5ad8833a6ce7cfa367651b1e8f0404677ea85792
daniasupriyadi <daniasupriyadi2106@gmail.com>
2022-05-29 13:13:05 +0700

    menambahkan form

--- lib/pages/pre_order.dart
+++ lib/pages/pre_order.dart
@@ -1,5 +1,6 @@
 import 'package:fashionizt/constants.dart';
 import 'package:fashionizt/theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PreOrder extends StatelessWidget {
@@ -22,6 +23,77 @@ class PreOrder extends StatelessWidget {
           style: titleApps,
         ),
       ),
-    );
+      body: Container(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Judul :', style: TextStyle(fontWeight: FontWeight.w600)),
+            const SizedBox(height: 5),
+            TextField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: BorderSide(width: 1)
+                ),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 15.0),),
+            Text('Kebutuhan Spesifikasi :', style: TextStyle(fontWeight: FontWeight.w600)),
+            const SizedBox(height: 5),
+            TextField(
+              maxLines: 5,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: BorderSide(width: 1)
+                ),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 15.0),),
+            Text('Budget :', style: TextStyle(fontWeight: FontWeight.w600)),
+            const SizedBox(height: 5),
+            TextField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: BorderSide(width: 1)
+                  ),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 15.0),),
+            Text('Lampiran :', style: TextStyle(fontWeight: FontWeight.w600)),
+            const SizedBox(height: 5),
+            TextField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(7),
+                      borderSide: BorderSide(width: 1)
+                  ),
+              ),
+            ),
+            SizedBox(height: 20,),
+
+            Card(
+              color: blacksand,
+              elevation: 5,
+              child: Container(
+                height: 40,
+                child: InkWell(
+                  splashColor: blush,
+                  onTap: (){},
+                  child: Center(
+                    child: Text("Submit", style: TextStyle(fontSize: 15, color: blush),),
+                  ),
+                ),
+              ),
+            )
+              ],
+            )
+        ),
+      );
   }
 }
