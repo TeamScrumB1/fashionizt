efa2b774fd5f140f3f436d409db3ad94922a4dfe
Izzudin Fasya <izzudinfasya@gmail.com>
2022-06-01 13:08:28 +0700

    filepicker + ganti warna status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:fashionizt/pages/login_screen.dart';
 import 'package:fashionizt/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fashionizt/constants.dart';
 
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: blacksand,
+    statusBarIconBrightness: Brightness.dark,
+  ));
   runApp(const MyApp());
 }
 
