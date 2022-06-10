7bc7025796d7e1419a5a7c4e7b4db29bd8b2e4bd
daniasupriyadi <daniasupriyadi2106@gmail.com>
2022-06-10 16:47:39 +0700

    menambahkan profile pic

--- lib/pages/list_project.dart
+++ lib/pages/list_project.dart
@@ -1,5 +1,6 @@
 import 'package:fashionizt/Widget/bottom_navbar.dart';
 import 'package:fashionizt/constants.dart';
+import 'package:fashionizt/pages/detail_project_desainer_mitra.dart';
 import 'package:fashionizt/pages/edit_myprofile.dart';
 import 'package:fashionizt/pages/login_screen.dart';
 import 'package:fashionizt/pages/pre_order.dart';
@@ -37,24 +38,94 @@ class _ProjectListState extends State<ProjectList>{
           },
         ),
         title: Text(
-          'My List Project',
+          'List Project',
           style: titleApps,
         ),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () async {
-
-            },
-            icon: const Icon(Icons.logout_rounded, size: 25,),
-            color: blush,
-          ),
-        ],
+        // actions: <Widget>[
+        //   IconButton(
+        //     onPressed: () async {
+        //
+        //     },
+        //     icon: const Icon(Icons.logout_rounded, size: 25,),
+        //     color: blush,
+        //   ),
+        // ],
       ),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
+        child: ListView.builder(
+          itemCount: 5,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) => Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.only(left: 15, right: 15),
+            margin: EdgeInsets.only(top: 10),
+            child: Card(
+              elevation: 1,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4.0),
+              ),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage('lib/Assets/images/profil.jpg'),
+                            radius: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("\nJudul Project", style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600)),
+                            Text("Spesifikasi : \nSaya ingin kaos saya bisa \ndibuat acara formal dan \njuga informal"),
+                            Text("Biaya : Rp 100.000", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: blacksand))
+                          ],
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    Column(
+                      children: [
+                        Text("24-10-2022", style: TextStyle(fontSize: 12, color: Colors.grey)),
+                        TextButton(
+                          onPressed: (){
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                                  return DetailProjectDesainer();
+                                }));
+                          },
+                          style: TextButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            backgroundColor: blacksand,
+                          ),
+                          child: Text("Detail", style: TextStyle(color: blush)),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+            ),
             ),
           ],
         ),
