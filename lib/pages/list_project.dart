433b799e9728899f75ccd35b5b4f7863f92e1528
daniasupriyadi <daniasupriyadi2106@gmail.com>
2022-06-12 22:13:53 +0700

    menambahkan profpic dan memindahkan button

--- lib/pages/list_project.dart
+++ lib/pages/list_project.dart
@@ -1,6 +1,7 @@
 import 'package:fashionizt/Widget/bottom_navbar.dart';
 import 'package:fashionizt/constants.dart';
 import 'package:fashionizt/pages/detail_project_desainer_mitra.dart';
+//import 'package:fashionizt/pages/detail_project_desainer_mitra.dart';
 import 'package:fashionizt/pages/edit_myprofile.dart';
 import 'package:fashionizt/pages/login_screen.dart';
 import 'package:fashionizt/pages/pre_order.dart';
@@ -52,90 +53,94 @@ class _ProjectListState extends State<ProjectList>{
         // ],
       ),
       body: SingleChildScrollView(
+        physics: ScrollPhysics(),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-        child: ListView.builder(
-          itemCount: 5,
-          shrinkWrap: true,
-          itemBuilder: (BuildContext context, int index) => Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.only(left: 15, right: 15),
-            margin: EdgeInsets.only(top: 10),
-            child: Card(
-              elevation: 1,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(4.0),
-              ),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          child: CircleAvatar(
-                            backgroundImage: AssetImage('lib/Assets/images/profil.jpg'),
-                            radius: 15,
-                          ),
-                        ),
-                      ],
+              child: ListView.builder(
+                itemCount: 5,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) => Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.only(left: 15, right: 15),
+                  margin: EdgeInsets.only(top: 10),
+                  child: Card(
+                    elevation: 1,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4.0),
                     ),
-                    Row(
-                      children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(" Anonymous", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600)),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text("\nJudul Project", style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600)),
-                            Text("Spesifikasi : \nSaya ingin kaos saya bisa \ndibuat acara formal dan \njuga informal"),
-                            Text("Biaya : Rp 100.000", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: blacksand))
-                          ],
-                        ),
-                      ],
-                    ),
-                    Spacer(),
-                    Column(
-                      children: [
-                        Text("24-10-2022", style: TextStyle(fontSize: 12, color: Colors.grey)),
-                        TextButton(
-                          onPressed: (){
-                            Navigator.push(context,
-                                MaterialPageRoute(builder: (context) {
-                                  return DetailProjectDesainer();
-                                }));
-                          },
-                          style: TextButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.only(right: 5, left: 5),
+                      margin: EdgeInsets.only(bottom: 8, top: 8),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(8),
+                            child: Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 18,
+                                  backgroundImage: AssetImage('lib/Assets/images/profil.jpg'),
+                                ),
+                                SizedBox(width: 10),
+                                Text("Anonymous", style: TextStyle(fontSize: 17,color: Colors.black, fontWeight: FontWeight.w500)),
+                                Spacer(),
+                                Text("24-06-2022", style: TextStyle(fontSize: 12, color: Colors.grey)),
+                              ],
                             ),
-                            backgroundColor: blacksand,
                           ),
-                          child: Text("Detail", style: TextStyle(color: blush)),
-                        ),
-                      ],
+                          Padding(
+                            padding: EdgeInsets.only(left: 8, right: 8),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text("Custom Kaos", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+                                    Text("Spesifikasi"),
+                                    Text("Biaya :", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: blacksand))
+                                  ],
+                                ),
+                                Spacer(),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 20),
+                                  child: Row(
+                                    children: [
+                                      Column(
+                                        children: [
+                                          TextButton(
+                                            onPressed: (){
+                                               Navigator.push(context,
+                                                   MaterialPageRoute(builder: (context) {
+                                                     return DetailProjectDesainer();
+                                                   }));
+                                            },
+                                            style: TextButton.styleFrom(
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(10.0),
+                                              ),
+                                              backgroundColor: blacksand,
+                                            ),
+                                            child: Text("Detail", style: TextStyle(color: blush)),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ),
-            ),
             ),
           ],
         ),
