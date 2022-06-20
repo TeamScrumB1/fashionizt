0b46f4143274eb67a62724c1df9403e23ff18863
TeamScrumbB1 <teamscrumb1@gmail.com>
2022-06-21 00:10:38 +0700

    Solving riwayat project

--- lib/api/api_project.dart
+++ lib/api/api_project.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../Models/project_model.dart';
+import '../variabels.dart';
 
 
 class ApiServiceProject {
   Future<Project> topHeadlines() async {
-    final response = await http.get(
-        Uri.parse('https://fashionizt.yufagency.com/project.php'));
+    final response = await http.post(
+        Uri.parse('https://fashionizt.yufagency.com/project.php'),body: {
+      "id_user" : IDUserGlob,
+    });
     if (response.statusCode == 200) {
       return Project.fromJson(json.decode(response.body));
     } else {
